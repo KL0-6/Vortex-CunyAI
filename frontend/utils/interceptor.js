8209486c26edddfe2af0851c5a69020691ec55c2
KL0-6 <145412851+KL0-6@users.noreply.github.com>
2026-04-24 20:20:12 -0400

    Revert "Cleaned up structure x2"
    This reverts commit 1c6f2b1c6636972db94d5bb0526a17c241771e50.

--- frontend/utils/interceptor.js
+++
@@ -1,6 +0,0 @@
-export function injectPageInterceptor() {
-  const script = document.createElement("script");
-  script.src = chrome.runtime.getURL("injected.js");
-  script.onload = () => script.remove();
-  (document.head || document.documentElement).prepend(script);
-}
